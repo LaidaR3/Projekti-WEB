546a2be1b1cd28c4a5f3fce771aa3a9304050a4e
Delfina Plakolli <delfinaplakolliii@gmail.com>
2024-01-22 12:26:35 -0700

    ..

--- rooms.php
+++ rooms.php
@@ -234,10 +234,10 @@
                 setTimeout("changeImg5()", 2000);
             }
             document.addEventListener(onload, changeImg5());
-            // Get the button:
+            
 let mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
+
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -248,10 +248,10 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  
+  document.documentElement.scrollTop = 0; 
 }
 
         </script>
