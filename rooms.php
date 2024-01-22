e0c91ed5aba895ae63409a086688aa895112c753
Delfina Plakolli <delfinaplakolliii@gmail.com>
2024-01-21 18:52:16 -0700

    button back to top at rooms

--- rooms.php
+++ rooms.php
@@ -12,6 +12,9 @@
 
     </head>
     <body>
+    <button onclick="topFunction()" id="myBtn" title="Go to top">	&#8963;</button>
+    
+
         <header class="headerContainer">
             <div class="logo">
                 <img src="imgs/logo1.png" alt="Logo">
@@ -231,6 +234,26 @@
                 setTimeout("changeImg5()", 2000);
             }
             document.addEventListener(onload, changeImg5());
+            // Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
         </script>
         
     </body>
