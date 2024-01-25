e4a92b91d60a06453923704ddf36ff8c2c02cdfb
LaidaR3 <rusinovcilaida13@gmail.com>
2024-01-26 00:21:38 +0100

    Dashboard Style

--- dashboard.php
+++ dashboard.php
@@ -4,11 +4,19 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="./style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
+<header class="headerContainer">
+    <div class="logo">
+        <img src="imgs/logo1.png" alt="Logo">
+    </div>
+    
+</header>
 <body>
-    <table border="1">
+    <table class="dashboard1">
              <tr>
                 <th>ID</th>
                  <th>NAME</th>
